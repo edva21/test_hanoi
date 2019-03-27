8099ed7e8fa258382d4f1e8caacd2504946d6caf
Eddy V <edva59012@outlook.com>
2019-03-27 10:55:20 -0600

    finished torre de Hannoi

--- program.py
+++ program.py
@@ -1,12 +1,13 @@
-def hanoi_test(quantity,pile_from,pile_to,pile_aux)
+def hanoi_test( quantity, pile_from, pile_to, pile_aux):
     if quantity ==1:
-        print "Mueve disco 1 de torre",pile_from," a torre ", pile_to
+        print ("Mueve disco 1 de torre",pile_from," a torre ", pile_to)
         return
     hanoi_test(quantity-1, pile_from,pile_aux,pile_to)
-        print "Mueve disco ",quantity," de torre",pile_from," a torre ", pile_to
+    print ("Mueve disco ",quantity," de torre",pile_from," a torre ", pile_to)
     hanoi_test(quantity-1, pile_aux,pile_to,pile_from)
 
 
     
-
+if __name__ == '__main__':
+    hanoi_test(4,'Borigen','Bdestino','Bintermedia')
 
