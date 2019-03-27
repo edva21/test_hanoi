93f13d0c78dfb8a4eec0794e10ce42bacd94a920
Eddy V <edva59012@outlook.com>
2019-03-27 10:41:09 -0600

    added hanoi_test function

--- program.py
+++ program.py
@@ -1,4 +1,12 @@
+def hanoi_test(quantity,pile_from,pile_to,pile_aux)
+    if quantity ==1:
+        print "Mueve disco 1 de torre",pile_from," a torre ", pile_to
+        return
+    hanoi_test(quantity-1, pile_from,pile_aux,pile_to)
+        print "Mueve disco ",quantity," de torre",pile_from," a torre ", pile_to
+    hanoi_test(quantity-1, pile_aux,pile_to,pile_from)
 
-pile1=[1,2,3,4]
-pile2=[]
-pile3=[]
+
+    
+
+
